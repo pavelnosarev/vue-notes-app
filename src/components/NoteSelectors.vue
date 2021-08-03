cce4768572038793dad47c1969fc5f75c8217a34
“Pavel <itspavelnosarev@gmail.com>
2021-08-03 15:29:46 -0400

    tried to fix :(

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="note-selectors"><NoteSelector v-for="note in notes" v-bind:note="note" :key="note.id"></div> thank
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function() {
+     return {
+	      notes: [
+	        { id: 1, body: "This is a first test", timestamp: Date.now() },
+	        { id: 2, body: "This is a second test", timestamp: Date.now() },
+	        { id: 3, body: "This is a third test", timestamp: Date.now() },
+	        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
+	      ]
+	    };
+
+  }
+};
+</script>
