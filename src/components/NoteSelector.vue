2beacaed3d38f4ce303f0011c8acee23902c8224
“Pavel <itspavelnosarev@gmail.com>
2021-08-03 16:05:13 -0400

    added componets , etc

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="note-selector active">
+  <div class="note-selector" v-bind:class="{ active: selectedNoteId === note.id }" v-on:click="selectNote(note)">
     <p class="note-selector-title">{{ note.body }}</p>
     <p class="note-selector-timestamp">{{ note.timestamp }}</p>
   </div>
@@ -7,6 +7,11 @@
 
 <script>
 export default {
-  props: ["note"],
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note);
+    },
+  },
 };
 </script>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="note-selectors"><NoteSelector v-for="note in notes" v-bind:note="note" :key="note.id"></div> thank
+  <div class="note-selectors">
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      :key="note.id"
+    />
+  </div>
 </template>
 
 <script>
 import NoteSelector from "./NoteSelector.vue";
-
 export default {
   components: {
     NoteSelector,
   },
-  data: function() {
-     return {
-	      notes: [
-	        { id: 1, body: "This is a first test", timestamp: Date.now() },
-	        { id: 2, body: "This is a second test", timestamp: Date.now() },
-	        { id: 3, body: "This is a third test", timestamp: Date.now() },
-	        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
-	      ]
-	    };
-
-  }
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "this is my first note", timestamp: Date.now() },
+        { id: 2, body: "this is my second note", timestamp: Date.now() },
+        { id: 3, body: "this is my third note", timestamp: Date.now() },
+        { id: 4, body: "this is my fourth note", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
+  },
 };
-</script>
